2dfd231c021442f735e5c4a40a13b42535fab438
Blake Wilson <blake@pushlabs.co>
2019-04-08 14:38:01 -0700

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,15 +56,15 @@ module.exports = {
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
-      // options: {
-      //   name: `Gatsby Starter Blog`,
-      //   short_name: `GatsbyJS`,
-      //   start_url: `/`,
-      //   background_color: `#ffffff`,
-      //   theme_color: `#663399`,
-      //   display: `minimal-ui`,
-      //   icon: `content/assets/gatsby-icon.png`
-      // }
+      options: {
+        name: `blake.id`,
+        short_name: `blake.id`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/icon.png`
+      }
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
