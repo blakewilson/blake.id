ac462789f584cdbe524dbfbb59f33bd62cb25ec0
Blake Wilson <blake@pushlabs.co>
2019-04-08 15:16:17 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-132058143-4`
       }
     },
     `gatsby-plugin-feed`,
