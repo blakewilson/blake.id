2ee53518873f8d27209466cfb3293e082181ae12
Blake Wilson <blake@blake.id>
2022-03-06 23:10:47 -0600

    Center videos in flow

--- components/postContent.tsx
+++ components/postContent.tsx
@@ -4,7 +4,7 @@ export interface PostContent {}
 
 export default function PostContent(props: PropsWithChildren<PostContent>) {
   return (
-    <div className="prose dark:prose-invert prose-lg md:prose-2xl mx-auto prose-pre:p-0 md:prose-pre:p-0 prose-h1:mb-6 md:prose-h1:mb-6">
+    <div className="prose dark:prose-invert prose-lg md:prose-2xl mx-auto prose-pre:p-0 md:prose-pre:p-0 prose-h1:mb-6 md:prose-h1:mb-6 prose-figure:text-center md:prose-figure:text-center prose-video:ml-auto prose-video:mr-auto">
       {props.children}
     </div>
   );
