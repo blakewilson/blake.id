c4a619b27339a59bd5a0f7d26ef93ebcc2916257
Blake Wilson <blake@blake.id>
2024-03-22 11:06:18 -0500

    Add projects

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -2,21 +2,32 @@ export type ProjectCardProps = {
   imageUrl: string;
   title: string;
   description: string;
+  projectUrl: string;
 };
 export function ProjectCard(props: ProjectCardProps) {
-  const { imageUrl, title, description } = props;
+  const { imageUrl, title, description, projectUrl } = props;
 
   return (
-    <article>
-      <img
-        src={imageUrl}
-        alt=""
-        className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover"
-      />
+    <article className="mb-16">
+      <div className="mb-4">
+        <a href={projectUrl}>
+          <img
+            src={imageUrl}
+            alt=""
+            className="aspect-[16/9] w-full rounded-2xl bg-gray-100"
+          />
+        </a>
+      </div>
 
-      <h2>{title}</h2>
+      <div className="mb-4">
+        <a href={projectUrl}>
+          <h2 className="text-2xl font-semibold leading-tight tracking-tight text-gray-900">
+            {title}
+          </h2>
+        </a>
+      </div>
 
-      <p>{description}</p>
+      <p className="text-lg text-slate-700">{description}</p>
     </article>
   );
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,9 +13,45 @@ export default function Projects() {
           <h2>Projects</h2>
 
           <ProjectCard
-            imageUrl="#"
+            imageUrl="/projects/insight-analytics.png"
+            projectUrl="https://insightanalytics.app"
+            title="Insight Analytics"
+            description="Insight Analytics seamlessly integrates with leading bookkeeping software platforms such as QuickBooks and Sage, transforming raw data into powerful dashboards for informed decision-making."
+          />
+
+          <ProjectCard
+            imageUrl="/projects/faustjs.png"
+            projectUrl="https://theunsentproject.com"
+            title="Faust.js"
+            description="While at WP Engine, I had the opportunity to help build and lead the development of the Faust.js Headless WordPress framework. Built on top of Next.js, the framework provides helpful features for interacting with a headless WordPress site like support for previews, authentication, sitemaps, template hierarchy system, and more."
+          />
+
+          <ProjectCard
+            imageUrl="/projects/the-unsent-project.png"
+            projectUrl="https://theunsentproject.com"
+            title="The Unsent Project"
+            description="I had the chance to work with Rora Blue on The Unsent Project to reinvent the publishing experience and overall site architecture. Using cloud services like AWS Lambda, RDS, API Gateway and Next.js/Vercel we were able to create an amazing UI/UX experience for users all while being scalable for large viral traffic events. The site grew from 50k submissions to over 7 million in under a year. The largest traffic spike was over 100m page views in a month."
+          />
+
+          <ProjectCard
+            imageUrl="/projects/video-background-pro.png"
+            projectUrl="https://pushlabs.co"
             title="Video Background Pro"
-            description="Testing"
+            description="A premium version of the Video Background plugin with advanced features and integrations like mobile video background playback, Vimeo and YouTube video backgrounds, integrations with WPBakery and SiteOrigin, and more."
+          />
+
+          <ProjectCard
+            imageUrl="/projects/video-background.png"
+            projectUrl="https://wordpress.org/plugins/video-background"
+            title="Video Background"
+            description="Video Background is a free WordPress plugin that easily allows you to add a video background to any section of your WordPress site by simply specifying a CSS selector."
+          />
+
+          <ProjectCard
+            imageUrl="/projects/directions-link-generator.png"
+            projectUrl="https://directionslinkgenerator.com"
+            title="Directions Link Generator"
+            description="Directions Link Generator lets you quickly enter a business name or address, then will generate a link for you to get directions from Google Maps to that location."
           />
         </div>
       </div>
